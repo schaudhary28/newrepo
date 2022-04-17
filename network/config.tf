e1ad7cf3dca17fd2b1ae3e682e290e0664d250f6
N697 <Nobelk@gmail.com>
2022-04-17 09:42:13 +0000

    Deploying networking module with trobleshoot code

---
+++ network/config.tf
@@ -0,0 +1,8 @@
+# S3 bucket for terraform state - dev networking
+terraform {
+  backend "s3" {
+    bucket = "dev-project-acs730"            // Bucket where to SAVE Terraform State
+    key    = "dev/network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
+    region = "us-east-1"                     // Region where bucket is created
+  }
+}
